fa9d214b64794bcf06ccbf11beb2538175e23fd4
Snowkids-code <oumaj47@gmail.com>
2022-11-09 22:38:26 +0300

    react router dom implementation done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
-import './App.css';
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import "./App.css";
+import LeftBar from "./components/layout/LeftBar";
+import Navbar from "./components/layout/Navbar";
+import RightBar from "./components/layout/RightBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
 
 function App() {
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <div style={{ flex: 6 }}>
+            <Outlet />
+          </div>
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
   return (
     <div className="App">
-      {/* <header className="App-header">
-        
-      </header> */}
-      <div>Hello</div>
+      <RouterProvider router={router} />
     </div>
   );
 }
